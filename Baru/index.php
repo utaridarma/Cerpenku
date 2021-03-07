ce9813ea3b16bddeddd020977bb2f1f652977ddd
utaridarma <79452493+utaridarma@users.noreply.github.com>
2021-03-07 11:29:40 +0800

    Add files via upload

---
+++ Baru/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <link rel:"stylesheet" href="css/style.css" <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="" stylesheet" type="text/css" href="css/style.css">
+    <title>YUK KE CERPENKU </title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info
+     fixed-top">
+        <a class="navbar-brand" href="#">CERPENKU</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Beranda<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Hari Ke 15</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kontak</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="media/form.html">Register</a>
+                </li>
+                
+            </ul>
+        </div>
+    </nav>
+    <section>
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="media/sakura gerak.gif" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="media/hujandeh.gif" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="media/hujann.gif" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
+    <div class="jumbotron" id="jumbotron">
+        <div class="container">
+            <div class="text-center">
+                <img src="media\okesip.jpg"> <width="300" class="rounded-circle img-thumbnail">
+                <h1 class="display-4">CERPENKU</h1>
+                <h3 class="lead">Cerita Tentang Kamu Aku Dan Semesta</h3>
+            </div>
+        </div>
+    </div>
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col text-center">
+                    <h2>Kutipan</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-5 text-justify">
+                    <p>Rayhan Rainy
+                        "Rainy? Putri Hujan dong "
+                         "aku menangis ? Apa begitu rindunya aku terhadapnya sampai aku memimpikannya dan menangis"</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Artikel Menarik lain nya-->
+    <section class="Artikel Menarik lain nya bg-light" id="Artikel Menarik lain nya">
+        <div class="container">
+            <div class="row pt-5 mb-4">
+                <div class="col text-center">
+                    <h2>Artikel Tari BALI</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md mb-4 text-justify">
+                    <div class="card">
+                        <img class="card-img-top" src="media/okesip.jpg" alt="HARI KE 15">
+                        <div class="card-body">
+                            <p class="card-text">Aku berdiri di depan balkon lantai dua sekolah ku sambil beberapa kali melihat segelintir siswa yang pulang terburu-buru karna akan turun hujan. Aku berjalan menyusuri balkon yang mungkin panjangnya hampir 5 meter itu, sambil memainkan jemariku sampai tak kusadari sekolah SMAku sudah sepi dengan murid dalam hati ku berbisik 
+                            "Oh dia belum kembali" aku pun berhenti dan bergegas mengambil tas yang masih aku taruh di dalam kelas, mungkin jika tasku dapat berbicara ia akan marah-marah terhadapku karena aku tinggalkan sendiri di dalam kelas yang sunyi dan gelap karena langit akan segera menjatuhkan rintik rintik hujannya.
+                            Aku menuruni tangga sambil ku hitung satu persatu tangga yg aku turuni tersebut, terlalu asik aku melamun tidak aku dengar suara yg memanggil dan menghampiriku, setelah tersadar dari lamunanku, aku melihat sosok mungil yg kiranya tingginya hanya 155 cm yang sedang dengan raut wajah marah melihatku, aku menatapnya sambil mengangkat alis seolah bertanya "kenapa" , ia mulai membuka suaranya yg nyaring seperti suara sirime ambulan yang tengah darurat 
+                            "Kamu kenapa sih, udah berminggu-minggu   kamu sering telat pulang sampek sekolah sepi kayak gini masih juga di sekolah, cinta banget sama sekolah kamu ya" ucapnya memarahi tapi bukan terlihat seram malah terlihat imut seperti badannya yg mungil 
+                            "Yang penting aku pulang teresa, daripada aku nginep di sekolah kan" ucapku bercanda yg langsung dapat pukulan kecil dari sahabatku itu 
+                            "Jangan bilang kamu masih nungguin pangeran mu itu, yang manggil kamu putri hujan" ucapnya yang seketika membuat senyuman dari bibirku berlahan menurun, aku mengangguk pasrah sambil terpaksa tersenyum menatap gadis mungil depanku itu, Teresa menepuk bahu ku sambil berkata 
+                            "Dia orang jahat rainy, buktinya dia ninggalin kamu kan setelah bilang bakal kembali, kamu orang baik rain dan kamu sahabat aku yg paling baik, berhenti nunggu dia, kamu harus kembali ke kehidupan kamu sebelum ngenal dia" ucap Teresa seakan meruntuhkan keyakinan ku, aku membantin dalam hati apa yang dikatakan teresa itu benar dia hanya orang jahat yang suka memainkan perempuan saja, apa aku salah satu korbannya  dan sekarang ia sedang bersenang-senang dengan perempuan lain, dan menghindari aku. Teresa kembali membuyarkan lamunan ku, dan mengandeng tanganku serta berkata 
+                            "Yuk pulang rain, keburu ujan kamu enggak mau kan nanti dimarah sama bundamu karna hujan-hujanan" ucapnya sambil mengandeng tangan ku untuk berjalan pulang.
+                            Sesampainya di rumah aku kembali teringat yang teresa katakan tadi di sekolah,  tapi aku merasa ia akan kembali perlakuannya terhadapku terlihat tulus dan tatapannya meneduhkan, aku tak percaya ia akan tega membohongiku, kembali aku lihat secarik kertas yang diberikan olehnya 2 minggu lalu ia menuliskan dengan jelas disana akan kembali di hari ke-10 setelah ia pamit untuk pergi tapi hari ini sudah hari ke-14 dan ia juga tak kembali, aku tak bisa mengubunginya, hati aku bergejolak aku merasakan dilema apakah akan berhenti menunggunya dan mengubur semua rasa yang ada. 
+                            "Putri hujan, kamu mau kemana heiii" aku melihatnya menatapku sambil memegang tangan ku, tatapannya masih sama meneduhkan dan senyumnya membuat diriku ingin memeluknya kencang dan berkata padanya jangan pergi lagi, aku melihat wajahnya pucat tapi tetap wajah yang aku favorit kan, tangan ku terangkat dan memegang wajahnya dan seketika berkata 
+                            "Kamu kembali?" ucapku  ia tersenyum dan memegang atas kepala ku 
+                            "Berhenti menungguku ya" ucapnya sambil memegang helaian rambut panjangnku, ia menepuk-nepuk atas rambutku seperti yg biasa ia lakukan, aku terdiam mendengar perkataannya apa maksudnya untuk berhenti menunggu sedangkan ia sudah kembali. Setelah ia berkata seakan ada sinar yg menyilaukan mataku, dan aku membuka mata aku berada di tempat tidur sambil memeluk erat surat yg pernah ia kirimkan kepadaku, aku merasakan pelupuk mataku berair dan dalam hatiku berbisik "aku menangis ? Apa begitu rindunya aku terhadapnya sampai aku memimpikannya dan menangis" aku menghela nafas dan mengangkat tubuhku untuk bangun dari ranjang, sekarang hari ke-15 setelah ia pergi.
+                            Kembali seperti hari-hari sebelumnya aku tetap menunggunya, aku mengingat saat pertama kali ia memanggil ku putri hujan, karna saat itu sedang turun hujan deras dan aku melihatnya berlari menuju kelasku untuk berteduh ia tidak sengaja menjatuhkan buku catatan  yg tengah aku bawa dan melihat nama ku di seragam SMA ku, ia berbisik 
+                            "Rainy? Putri Hujan dong" ia pun tertawa, tertawanya keras yg membuatku kesal dan marah terhadapnya, buku catatan ku basah tetapi ia dan ia merasa bersalah ia berjanji untuk mencatat ulang catatanku yang basah, sejak itu ia sering berada di balkon depan kelas ku untuk menanyakan apa isi catatanku itu dan kita pun mulai dekat sejalannya waktu sampai ia tiba-tiba berkata akan pergi dan mengirimi aku surat. 
+                            Aku tersenyum mengingat kejadian indah itu, seakan-akan tetap ingin merasakannya kembali sampai saat aku kembali ke kelas untuk mengambil tas untuk pulang, tak sengaja aku melihat surat yang terselip di sela-sela buku yang aku rapikan,aku berjalan menuruni tangga sambil membaca kata perkata surat itu aku menegang, pandanganku langsung tertuju di depan ku lihat badan tegak tinggi berada di depan ku sambil menebarkan senyum manisnya, 
+                            "Ternyata benar, putri hujan" ucapnya sambil  tersenyum dan kembali menjatuhkan pertanyaan 
+                            "Kamu sendirian?" Aku masih terdiam dan masih menatapnya dalam-dalam ingin sekali berkata kepadanya bahwa aku menunggunya, bahwa sudah berhari-hari aku menantikan kehadirannya kembali, dia ikut terdiam aku menangis dihadapannya aku menangis sambil ingin memegang wajahnya, berlahan-lahan wajah yang aku nantikan selama berhari-hari menghilang, tangisan ku semakin menjadi-jadi dibawah rintihan hujan aku tertunduk dan terjatuh. Aku ingin berteriak dibawah rintihan hujan aku ingin memaki-makinya mengapa meninggalkan diriku dengan cara seperti ini  ia berjanji kembali apa ini maksud dari kedatangannya kemarin dan mengapa harus menyampailkannya setelah ia tiada.
+                            “Kamu sudah enggak sakit lagi ya Rayhan, sekarang aku yang ngerasa sakit atas kepergianmu yang tiba-tiba tapi gapapa ray aku ikhlasin kamu . Dateng lagi ke mimpi aku ya sekali aja, aku kangen kamu bener-bener kangen kamu ray” Ucapku terbata-bata sambil menahan tangis melihat batu nisan yang bertuliskan nama orang yang aku tunggu selama ini, ia telah meninggalkan aku dan juga kenangan indah yang pernah dibuatnya.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+                        <div class="col-lg-3">
+                <div class="sidebar">
+                    <div class="row">
+                        <div class="berita col-lg-12">
+                            <div class="sidebar-title">
+</body>
+</html>
